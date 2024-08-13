86ed2ebc9aa334ff4b9f170777229396b4dcc1fd
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-14 01:46:28 +0700

    add method for get database url

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,30 +1,51 @@
 package hexlet.code;
 
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 public class App {
     public static void main(String[] args) {
-
-        var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
-
-        var dataSource = new HikariDataSource(hikariConfig);
-
         Javalin app = App.getApp();
         app.start(getPort());
     }
 
+    private static String getDatabaseUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+    }
+
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System.getenv().getOrDefault("DB_PORT", "7070");
         return Integer.valueOf(port);
     }
 
-    public static Javalin getApp() {
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    public static Javalin getApp() throws IOException {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDatabaseUrl());
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        BaseRepository.dataSource = dataSource;
+
+        var sql = readResourceFile("schema.sql");
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
+
         app.get("/", ctx -> ctx.result("Hello World"));
         return app;
     }
