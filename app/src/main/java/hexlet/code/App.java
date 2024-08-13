dc83c821cf92451ea970fca0110c47e8dda28c0a
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-14 02:57:19 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,11 +5,11 @@ import io.javalin.Javalin;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-//import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.nio.charset.StandardCharsets;
-//import java.util.stream.Collectors;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws IOException {
@@ -26,14 +26,12 @@ public class App {
         return Integer.valueOf(port);
     }
 
-    /*
     private static String readResourceFile(String fileName) throws IOException {
         var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             return reader.lines().collect(Collectors.joining("\n"));
         }
     }
-     */
 
     public static Javalin getApp() throws IOException {
         var hikariConfig = new HikariConfig();
