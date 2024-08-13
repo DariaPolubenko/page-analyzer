383b256134ec4945efc88da6d0905b586c819d14
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-14 06:54:19 +0700

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,18 +22,26 @@ repositories {
 }
 
 dependencies {
-    implementation("com.h2database:h2:2.2.220")
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.postgresql:postgresql:42.1.4")
-    implementation("io.javalin:javalin:6.1.3")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("io.javalin:javalin-rendering:6.1.3")
-    implementation("gg.jte:jte:3.1.9")
-    testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.0-M1")
     testImplementation ("org.junit.jupiter:junit-jupiter-params:5.8.0-M1")
-    testImplementation ("org.assertj:assertj-core:3.19.0")
+
+    implementation("com.h2database:h2:2.2.224")
+    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("org.apache.commons:commons-text:1.11.0")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+
+    testImplementation("org.assertj:assertj-core:3.25.2")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation ("com.squareup.okhttp3:mockwebserver:4.12.0")
+    implementation ("org.jsoup:jsoup:1.17.2")
+    implementation ("com.konghq:unirest-java:3.13.0")
 }
 
 tasks.test {
