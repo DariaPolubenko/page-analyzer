69dfb9ec45f9ab76f288cd95616e4ab622723788
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-14 19:48:31 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -17,33 +17,17 @@ import lombok.extern.slf4j.Slf4j;
 public class App {
     public static void main(String[] args) throws IOException, SQLException {
         Javalin app = App.getApp();
-        app.start(getPort());
-    }
-
-    private static String getDatabaseUrl() {
-        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
-    }
-
-    private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
-        return Integer.valueOf(port);
-    }
-
-    private static String readResourceFile(String fileName) throws IOException {
-        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-            return reader.lines().collect(Collectors.joining("\n"));
-        }
+        app.start(Utils.getPort());
     }
 
     public static Javalin getApp() throws IOException, SQLException {
         var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(getDatabaseUrl());
+        hikariConfig.setJdbcUrl(Utils.getDatabaseUrl());
 
         var dataSource = new HikariDataSource(hikariConfig);
         BaseRepository.dataSource = dataSource;
 
-        var sql = readResourceFile("schema.sql");
+        var sql = Utils.readResourceFile("schema.sql");
         log.info(sql);
         try (var connection = dataSource.getConnection();
              var statement = connection.createStatement()) {
