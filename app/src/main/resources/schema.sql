21b5130731de1957a7d2d4bd7494ab26f2f6e5a9
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-14 19:39:13 +0700

    update schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    created_at TIMESTAMP
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
 );
