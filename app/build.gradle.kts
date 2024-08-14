9f0e1d90af71f13f49e2871b0dfd4f47a42084e1
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-14 19:57:05 +0700

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,23 +22,22 @@ repositories {
 }
 
 dependencies {
-    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.0-M1")
-    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.8.0-M1")
-
     implementation("com.h2database:h2:2.2.224")
     implementation("org.postgresql:postgresql:42.7.1")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("org.apache.commons:commons-text:1.11.0")
-    implementation("gg.jte:jte:3.1.9")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("gg.jte:jte:3.1.12")
     implementation("io.javalin:javalin:6.1.3")
     implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 
     testImplementation("org.assertj:assertj-core:3.25.2")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.0-M1")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.8.0-M1")
     testImplementation ("com.squareup.okhttp3:mockwebserver:4.12.0")
     implementation ("org.jsoup:jsoup:1.17.2")
     implementation ("com.konghq:unirest-java:3.13.0")
