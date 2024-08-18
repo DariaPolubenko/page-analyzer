4d3c2dc62fdf7d3f4fc5eb8c3829d0b93a965448
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-19 05:46:54 +0700

    update UrlsController.java

--- app/src/main/java/hexlet/code/controllers/UrlsController.java
+++ app/src/main/java/hexlet/code/controllers/UrlsController.java
@@ -7,9 +7,14 @@ import hexlet.code.repository.UrlRepository;
 import hexlet.code.utils.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.validation.ValidationException;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.net.URI;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
@@ -19,28 +24,29 @@ public class UrlsController {
         ctx.render("index.jte", model("page", page));
     }
 
-    public static void create(Context ctx) throws SQLException {
-        try {
-            var name = ctx.formParamAsClass("name", String.class)
-                    .check(v -> {
-                        try {
-                            return UrlRepository.search(v).isEmpty();
-                        } catch (SQLException e) {
-                            throw new RuntimeException(e);
-                        }
-                    },"Сайт с таким адресом уже существует")
-                    .get();
+    public static void create(Context ctx) throws SQLException, URISyntaxException, MalformedURLException {
+        var name = ctx.formParam("name");
+        var url = new URI(name).toURL();
 
+        var normalizedUrl = UriComponentsBuilder.newInstance()
+                .scheme(url.getProtocol())
+                .host(url.getHost())
+                .port(url.getPort())
+                .build()
+                .toUri()
+                .toURL()
+                .toString();
+
+        if (UrlRepository.search(normalizedUrl).isEmpty()) {
             Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
-            var url = new Url(name, timestamp);
-            UrlRepository.save(url);
+            var resultUrl = new Url(normalizedUrl, timestamp);
+            UrlRepository.save(resultUrl);
             ctx.sessionAttribute("flash", "Сайт добавлен!");
             ctx.redirect(NamedRoutes.urlsPath());
 
-        } catch (ValidationException e) {
-            var name = ctx.formParam("name");
-            var page = new BuildUrlsPage(name, e.getErrors());
+        } else {
+            var page = new BuildUrlsPage(normalizedUrl, "Сайт с таким адресом уже существует");
             ctx.render("index.jte", model("page", page));
         }
     }
