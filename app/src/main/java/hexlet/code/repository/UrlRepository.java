18b17bf83569000ccff450590cca9059e5ce3a58
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-19 23:23:02 +0700

    add method 'find' in UrlRepository.java

--- app/src/main/java/hexlet/code/repository/UrlRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlRepository.java
@@ -28,6 +28,25 @@ public class UrlRepository  extends BaseRepository {
         }
     }
 
+    public static Optional<Url> find(Long id) throws SQLException {
+        var sql = "SELECT * FROM urls WHERE id = ?";
+
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            var resultSet = stmt.executeQuery();
+
+            if (resultSet.next()) {
+                var name = resultSet.getString("name");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var url = new Url(name, createdAt);
+                url.setId(id);
+                return Optional.of(url);
+            }
+            return Optional.empty();
+        }
+    }
+
     public static List<Url> getEntities() throws SQLException {
         var sql = "SELECT * FROM urls";
 
