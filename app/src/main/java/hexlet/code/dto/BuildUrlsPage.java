797d444f2f093f8f6ced9d815e0ac02ec4e5585f
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-19 23:24:27 +0700

    update BuildUrlsPage.java

--- app/src/main/java/hexlet/code/dto/BuildUrlsPage.java
+++ app/src/main/java/hexlet/code/dto/BuildUrlsPage.java
@@ -1,12 +1,8 @@
 package hexlet.code.dto;
 
-import io.javalin.validation.ValidationError;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.Map;
 
 @AllArgsConstructor
 @NoArgsConstructor
