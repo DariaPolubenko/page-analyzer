2af5209c376405e67f95a0a122c0838d04542d21
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-19 23:24:01 +0700

    add paths in NamedRoutes.java

--- app/src/main/java/hexlet/code/utils/NamedRoutes.java
+++ app/src/main/java/hexlet/code/utils/NamedRoutes.java
@@ -8,5 +8,13 @@ public class NamedRoutes {
     public static String urlsPath() {
         return "/urls";
     }
+
+    public static String urlsPath(String id) {
+        return "/urls/" + id;
+    }
+
+    public static String urlsPath(Long id) {
+        return urlsPath(String.valueOf(id));
+    }
 }
 
