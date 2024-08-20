85bd1549f5cf3cedd6b607ce82e63eeb4a3fd8cb
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-21 01:15:12 +0700

    update AppTest.java

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,36 @@
+package hexlet.code;
+
+import hexlet.code.utils.NamedRoutes;
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AppTest {
+    private static Javalin app;
+
+    @BeforeAll
+    public static void beforeAll() throws IOException, SQLException {
+        app = App.getApp();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
+    }
+
+    @Test
+    public void testMainPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.mainPath());
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+}
