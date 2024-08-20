a4d40d4b02cfd503b82d0f0e28a4ea2064b940ff
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-21 06:35:42 +0700

    update App.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.repository.UrlRepository;
 import hexlet.code.utils.NamedRoutes;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
@@ -47,11 +48,32 @@ public class AppTest {
     @Test
     public void testCreateUrl() {
         JavalinTest.test(app, (server, client) -> {
-            var requestBody = "name=https://getbootstrap.com/docs/5.3/components/buttons/";
+            var requestBody1 = "name=https://getbootstrap.com/docs/5.3/components/buttons/";
+            var expected1 = "https://getbootstrap.com";
 
-            var response1 = client.post(NamedRoutes.urlsPath(), requestBody);
+            var response1 = client.post(NamedRoutes.urlsPath(), requestBody1);
             assertThat(response1.code()).isEqualTo(200);
 
+            var actual1 = UrlRepository.search(expected1).get().getId();
+            assertThat(actual1).isEqualTo(1);
+
+            var actual12 = UrlRepository.search(expected1).get().getName();
+            assertThat(actual12).isEqualTo(expected1);
+
+/*
+            var requestBody2 = "http://localhost:7070/";
+            var expected2 = "http://localhost:7070";
+
+            var response21 = client.post(NamedRoutes.urlsPath(), requestBody2);
+            assertThat(response21.code()).isEqualTo(200);
+
+            var actual22 = UrlRepository.search(expected2).get().getId();
+            assertThat(actual22).isEqualTo(2);
+
+            var actual23 = UrlRepository.search(expected2).get().getName();
+            assertThat(actual23).isEqualTo(expected2);
+
+ */
             var response2 = client.get(NamedRoutes.urlsPath());
             assertThat(response2.code()).isEqualTo(200);
 
