01460452fb1160b93d9f4dc5ba8f45fe98707c4f
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-21 01:57:40 +0700

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -5,6 +5,7 @@ import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -15,14 +16,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class AppTest {
     private static Javalin app;
 
-    @BeforeAll
-    public static void beforeAll() throws IOException, SQLException {
+    @BeforeEach
+    public final void setUp() throws IOException, SQLException {
         app = App.getApp();
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        app.stop();
     }
 
     @Test
@@ -33,4 +29,20 @@ public class AppTest {
         });
     }
 
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlsPath());
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    public void testUrlNotFound() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlsPath(Long.valueOf(1)));
+            assertThat(response.code()).isEqualTo(404);
+            assertThat(response.body().string()).contains("Сайт не найден");
+        });
+    }
 }
