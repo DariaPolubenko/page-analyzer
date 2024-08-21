569c78058349d8057185c5e70ed13f71f86cf253
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-22 05:47:53 +0700

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -49,36 +49,31 @@ public class AppTest {
     public void testCreateUrl() {
         JavalinTest.test(app, (server, client) -> {
             var requestBody1 = "name=https://getbootstrap.com/docs/5.3/components/buttons/";
+            var requestBody2 = "name=https://java-page-analyzer-ru.hexlet.app/urls";
+
             var expected1 = "https://getbootstrap.com";
+            var expected2 = "https://java-page-analyzer-ru.hexlet.app";
+
 
             var response1 = client.post(NamedRoutes.urlsPath(), requestBody1);
             assertThat(response1.code()).isEqualTo(200);
 
+            var response2 = client.post(NamedRoutes.urlsPath(), requestBody2);
+            assertThat(response2.code()).isEqualTo(200);
+
+
             var actual1 = UrlRepository.search(expected1).get().getId();
             assertThat(actual1).isEqualTo(1);
 
-            var actual12 = UrlRepository.search(expected1).get().getName();
-            assertThat(actual12).isEqualTo(expected1);
+            var actual2 = UrlRepository.search(expected2).get().getId();
+            assertThat(actual2).isEqualTo(2);
 
-/*
-            var requestBody2 = "http://localhost:7070/";
-            var expected2 = "http://localhost:7070";
 
-            var response21 = client.post(NamedRoutes.urlsPath(), requestBody2);
-            assertThat(response21.code()).isEqualTo(200);
-
-            var actual22 = UrlRepository.search(expected2).get().getId();
-            assertThat(actual22).isEqualTo(2);
-
-            var actual23 = UrlRepository.search(expected2).get().getName();
-            assertThat(actual23).isEqualTo(expected2);
-
- */
-            var response2 = client.get(NamedRoutes.urlsPath());
-            assertThat(response2.code()).isEqualTo(200);
-
-            var response3 = client.get(NamedRoutes.urlsPath(Long.valueOf(1)));
+            var response3 = client.get(NamedRoutes.urlsPath());
             assertThat(response3.code()).isEqualTo(200);
+
+            var response4 = client.get(NamedRoutes.urlsPath(Long.valueOf(1)));
+            assertThat(response4.code()).isEqualTo(200);
         });
     }
 }
