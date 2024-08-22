c2769344a779a154558b7f83fe420b6ca8726902
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-23 00:39:44 +0700

    create UrlCheck.java

---
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -0,0 +1,11 @@
+package hexlet.code.model;
+
+public class UrlCheck {
+    private Long id;
+    private int statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private Long urlId;
+
+}
