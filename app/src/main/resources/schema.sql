36899b45709a7617ee2b79122514ed6c9bdf210e
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-23 00:39:55 +0700

    create schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -5,3 +5,14 @@ CREATE TABLE urls (
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY NOT NULL,
+    url_id BIGINT NOT NULL,
+    status_code INTEGER NOT NULL,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP NOT NULL
+
+);
