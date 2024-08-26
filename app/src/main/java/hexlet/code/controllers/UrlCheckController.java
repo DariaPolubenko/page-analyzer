6a9256b5d56b213e0633cec86a5b0348b0f62eb2
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-27 04:31:37 +0700

    update UrlChecksController.java

--- app/src/main/java/hexlet/code/controllers/UrlCheckController.java
+++ app/src/main/java/hexlet/code/controllers/UrlCheckController.java
@@ -1,13 +1,13 @@
 package hexlet.code.controllers;
 
-import hexlet.code.dto.UrlCheckPage;
+import hexlet.code.dto.UrlPage;
 import hexlet.code.model.UrlCheck;
 import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlRepository;
+import hexlet.code.utils.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -17,27 +17,28 @@ import java.sql.Timestamp;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
-public class UrlChecksController {
+public class UrlCheckController {
 
     public static void checkUrl(Context ctx) throws SQLException {
         var id = ctx.pathParamAsClass("id", Long.class).get();
         var url = UrlRepository.find(id).orElseThrow(() -> new NotFoundResponse("Сайт не найден"));
+        var check = getCheck(url.getName(), id);
+        UrlCheckRepository.saveCheck(check);
+        ctx.redirect(NamedRoutes.urlsPath(id));
+    }
 
-        HttpResponse<String> response = Unirest.get(url.getName()).asString();
+    public static UrlCheck getCheck(String url, Long id) throws SQLException {
+        HttpResponse<String> response = Unirest.get(url).asString();
 
         var statusCode = response.getStatus();
         var body = response.getBody();
 
-        //Document doc = Jsoup.parse(body);
-        //var title = doc.title();
-        //var h1 = doc.select("h1").toString();
-        //var description = doc.select("meta[name=description]").attr("content");
+        Document doc = Jsoup.parse(body);
+        var title = doc.title();
+        var h1 = doc.select("h1").toString();
+        var description = doc.select("meta[name=description]").attr("content");
         var createdAt = new Timestamp(System.currentTimeMillis());
 
-        var check = new UrlCheck(statusCode, "", "", "", id, createdAt);
-        UrlCheckRepository.save(check);
-
-        var checkPage = new UrlCheckPage(check);
-        ctx.render("show.jte", model("checkPage", checkPage));
+       return new UrlCheck(statusCode, title, h1, description, id, createdAt);
     }
 }
