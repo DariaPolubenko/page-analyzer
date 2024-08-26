e8af978ba705f789d97ed4f1505dd5f6a29c0b86
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-26 22:53:44 +0700

    create UrlCheckPage.java

---
+++ app/src/main/java/hexlet/code/dto/UrlCheckPage.java
@@ -0,0 +1,11 @@
+package hexlet.code.dto;
+
+import hexlet.code.model.UrlCheck;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UrlCheckPage {
+    private UrlCheck urlCheck;
+}
