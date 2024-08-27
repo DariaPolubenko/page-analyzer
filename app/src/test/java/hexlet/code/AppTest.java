58b3a11d51d0fded0e00fa915f1c0ccab6fdb035
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-28 02:17:08 +0700

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -4,6 +4,8 @@ import hexlet.code.repository.UrlRepository;
 import hexlet.code.utils.NamedRoutes;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -29,7 +31,7 @@ public class AppTest {
     }
 
     @Test
-    public void testUrlsPage1() {
+    public void testUrlsPage() {
         JavalinTest.test(app, (server, client) -> {
             var response = client.get(NamedRoutes.urlsPath());
             assertThat(response.code()).isEqualTo(200);
@@ -37,7 +39,7 @@ public class AppTest {
     }
 
     @Test
-    public void testUrlNotFound() {
+    public void testUrlPageNotFound() {
         JavalinTest.test(app, (server, client) -> {
             var response = client.get(NamedRoutes.urlsPath(Long.valueOf(1)));
             assertThat(response.code()).isEqualTo(404);
@@ -48,32 +50,30 @@ public class AppTest {
     @Test
     public void testCreateUrl() {
         JavalinTest.test(app, (server, client) -> {
-            var requestBody1 = "name=https://getbootstrap.com/docs/5.3/components/buttons/";
-            var requestBody2 = "name=https://java-page-analyzer-ru.hexlet.app/urls";
+            var requestBody = "name=https://getbootstrap.com/docs/5.3/components/buttons/";
+            var expected = "https://getbootstrap.com";
 
-            var expected1 = "https://getbootstrap.com";
-            var expected2 = "https://java-page-analyzer-ru.hexlet.app";
-
-
-            var response1 = client.post(NamedRoutes.urlsPath(), requestBody1);
+            var response1 = client.post(NamedRoutes.urlsPath(), requestBody);
             assertThat(response1.code()).isEqualTo(200);
+            assertThat(response1.body().string()).contains("https://getbootstrap.com");
 
-            var response2 = client.post(NamedRoutes.urlsPath(), requestBody2);
-            assertThat(response2.code()).isEqualTo(200);
-
-
-            var actual1 = UrlRepository.search(expected1).get().getId();
+            var actual1 = UrlRepository.search(expected).get().getId();
             assertThat(actual1).isEqualTo(1);
 
-            var actual2 = UrlRepository.search(expected2).get().getId();
-            assertThat(actual2).isEqualTo(2);
-
-
-            var response3 = client.get(NamedRoutes.urlsPath());
+            var response3 = client.get(NamedRoutes.urlsPath(Long.valueOf(1)));
             assertThat(response3.code()).isEqualTo(200);
-
-            var response4 = client.get(NamedRoutes.urlsPath(Long.valueOf(1)));
-            assertThat(response4.code()).isEqualTo(200);
         });
     }
+/*
+    @Test
+    public void testUrlCheck() {
+        var server = new MockWebServer();
+        var url = server.url("https://www.google.com").toString();
+
+        var response = new MockResponse()
+                .
+
+    }
+
+ */
 }
